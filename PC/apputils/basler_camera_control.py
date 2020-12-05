a8aedca1a2dc6e393697303bc892898b1ce7c91f
Marti-R <marti.r@web.de>
2020-12-05 20:49:13 +0100

    Functional BaslerCameraProcess.

--- PC/apputils/basler_camera_control.py
+++ PC/apputils/basler_camera_control.py
@@ -8,7 +8,4 @@ class BaslerCameraControl:
 
     def update_cameras(self):
         TlFactory = pylon.TlFactory.GetInstance()
-        available_devices = TlFactory.EnumerateDevices()
-
-        for device_index, device in enumerate(available_devices):
-            self.cameras[device_index] = TlFactory.CreateDevice(device)
+        self.cameras = TlFactory.EnumerateDevices()
